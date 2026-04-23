722d73a1717c425ca3701126ed2a662337c8e558
Adamm0212 <adomas.pimpe@gmail.com>
2026-04-23 15:01:08 +0300

    Added more coherent test to test.cpp

--- test.cpp
+++ test.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <cassert>
 #include <fstream>
+#include <string>
 
-using namespace gap_buffer;
+using namespace Gap_buffer;
 
 void log(std::ostream& os, std::ofstream& file, const std::string& message) {
     os << message << std::endl;
@@ -18,43 +19,41 @@ int main() {
     try {
         GapBuffer gb;
         log(std::cout, logFile, "Test 1: Adding characters");
-        log(std::cout, logFile, "Adding 'H' to the buffer...");
-        gb += 'H';
-        log(std::cout, logFile, "Adding 'e' to the buffer...");
-        gb += 'e';
-        log(std::cout, logFile, "Adding 'l' to the buffer...");
-        gb += 'l';
-        log(std::cout, logFile, "Adding 'l' to the buffer...");
-        gb += 'l';
-        log(std::cout, logFile, "Adding 'o' to the buffer...");
+        log(std::cout, logFile, "Building the initial text: Code");
+        gb += 'C';
         gb += 'o';
-        assert(gb.toString() == "Hello (size: 5/10)");
+        gb += 'd';
+        gb += 'e';
+        assert(gb.toString() == "Code (size: 4/10)");
         log(std::cout, logFile, "PASSED: " + gb.toString());
 
-        log(std::cout, logFile, "Test 2: Moving cursor");
-        gb.moveCursor(1);
-        log(std::cout, logFile, "Cursor moved to position 1");
-        gb += 'a';
-        log(std::cout, logFile, "Inserted 'a' at cursor position");
+        log(std::cout, logFile, "Test 2: Moving cursor and inserting");
+        gb.moveCursor(2);
+        log(std::cout, logFile, "Cursor moved between 'o' and 'd'");
+        gb += '-';
+        log(std::cout, logFile, "Inserted '-' at cursor position");
 
-        assert(gb.toString() == "Haello (size: 6/10)");
+        assert(gb.toString() == "Co-de (size: 5/10)");
         log(std::cout, logFile, "PASSED: " + gb.toString());
 
         log(std::cout, logFile, "Test 3: Testing update operator *=");
-        gb *= 'A';
-        assert(gb.toString() == "HaAllo (size: 6/10)");
+        log(std::cout, logFile, "Replacing the character at the cursor: d -> D");
+        gb *= 'D';
+        assert(gb.toString() == "Co-De (size: 5/10)");
         log(std::cout, logFile, "PASSED: " + gb.toString());
 
         log(std::cout, logFile, "Test 4: Testing search operator []");
-        assert(gb['A'] == 2);
+        assert(gb['D'] == 3);
         log(std::cout, logFile, "PASSED: " + gb.toString());
 
         log(std::cout, logFile, "Test 5: Testing delete operator -=");
+        log(std::cout, logFile, "Deleting the character before the cursor: '-'");
         gb -= 1;
-        assert(gb.toString() == "HAllo (size: 5/10)");
+        assert(gb.toString() == "CoDe (size: 4/10)");
         log(std::cout, logFile, "PASSED: " + gb.toString());
 
         log(std::cout, logFile, "Test 6: Testing deep copy");
+        gb.moveCursor(4);
         GapBuffer gb2 = gb;
         gb2 += '!';
         assert(gb.toString().find("!") == std::string::npos);
@@ -72,13 +71,24 @@ int main() {
         log(std::cout, logFile, "Test 8: Testing comparison operators");
         assert(gb != gb2);
         assert(gb < gb2);
+
+        GapBuffer cat;
+        cat += 'c';
+        cat += 'a';
+        cat += 't';
+
+        GapBuffer dog;
+        dog += 'd';
+        dog += 'o';
+        dog += 'g';
+
+        assert(cat < dog);
         log(std::cout, logFile, "PASSED: " + gb.toString());
 
         log(std::cout, logFile, "Test 9: Testing clear operator !");
         !gb;
         assert(gb.toString() == " (size: 0/10)");
         log(std::cout, logFile, "PASSED: " + gb.toString());
-        log(std::cout, logFile, "----------------------------------------");
 
         log(std::cout, logFile, "Test 10: Testing out of bounds");
         try {
@@ -88,23 +98,22 @@ int main() {
         catch (const std::exception& e) {
             log(std::cout, logFile, "PASSED: Caught expected exception: " + std::string(e.what()));
         }
-        log(std::cout, logFile, "----------------------------------------");
 
         log(std::cout, logFile, "Test 11: Searching for a missing character");
         assert(gb['z'] == -1);
         log(std::cout, logFile, "PASSED: missing character returns -1");
-        log(std::cout, logFile, "----------------------------------------");
 
         log(std::cout, logFile, "Test 12: Reusing cleared buffer");
-        gb += 'X';
-        gb += 'Y';
-        assert(gb.toString() == "XY (size: 2/10)");
+        gb += 'O';
+        gb += 'K';
+        assert(gb.toString() == "OK (size: 2/10)");
+        
         log(std::cout, logFile, "PASSED: " + gb.toString());
     }
     catch (const std::exception& e) {
         log(std::cout, logFile, std::string("Test failed with exception: ") + e.what());
     }
-
+    log(std::cout, logFile, "----------------------------------------");
     log(std::cout, logFile, "ALL TESTS COMPLETED");
     logFile.close();
     return 0;
